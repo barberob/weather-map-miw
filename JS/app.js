7fdd812357a4959293537b13a39fb7ea2cbf7914
Théo Meozzi <54541555+theomeozzi@users.noreply.github.com>
2021-01-05 11:55:43 +0100

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,13 @@
+// On créé une carte OpenStreetMap avec la librairie Leaflet
+
+let carte = L.map('maCarte').setView([44.55962000171788, 6.079823238576286], 12);
+
+// On lui applique un style de tuiles
+
+L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+//Il est toujours bien de laisser le lien vers la source des données
+
+attribution: 'données <a href="https://www.openstreetmap.org/copyright"> OpenStreetMap </a> <span> & </span> <a href="https://www.openstreetmap.fr/"> OSM France </a>',
+minZoom: 1,
+maxZoom: 20,
+}).addTo(carte);
