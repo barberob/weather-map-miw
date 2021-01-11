e461c03d80eafd9a57fd49c88c51d943d78922a7
michelgege <michelgege@pop-os.localdomain>
2021-01-11 14:16:14 +0100

    auth by client credentials

--- connect.php
+++ connect.php
@@ -2,8 +2,6 @@
 
 require_once('config.php');
 
-$code = $_GET['code'];
-
 $curl = curl_init();
 
 curl_setopt_array($curl, [
@@ -14,7 +12,7 @@ curl_setopt_array($curl, [
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "grant_type=".$grant_type."&client_id=".$client_id."&client_secret=".$client_secret."&code=".$code."&redirect_uri=http%3A%2F%2Flocalhost%2Fmap.php&scope=".$scope,
+  CURLOPT_POSTFIELDS => "grant_type=".$grant_type."&client_id=".$client_id."&client_secret=".$client_secret."&username=".$username."&password=".$password,
   CURLOPT_HTTPHEADER => [
     "Content-Type: application/x-www-form-urlencoded;charset=UTF-8",
     "Host: api.netatmo.com"
@@ -29,5 +27,10 @@ curl_close($curl);
 if ($err) {
   echo "cURL Error #:" . $err;
 } else {
-  echo $response;
+  $response = json_decode($response);
+  // echo '<pre>';
+  // var_dump($response->access_token);
+  // echo '</pre>';
+  // die();
+  header('Location: map.php?token='.$response->access_token);
 }
