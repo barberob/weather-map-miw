a18bc9aeee8c38caa3690ad9667c8f5447e2136a
michelgege <barberot.b@gmail.com>
2021-01-25 13:53:52 +0100

    add layer group lib

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
                  integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
                  crossorigin=""/>
+        <link rel="stylesheet" type="text/css" href="css/marker_group.css">
          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     </head>
     <body>
@@ -23,6 +24,7 @@
                    crossorigin=""
                    type="text/javascript">
         </script>
+        <script src="js/marker_group.js"></script>
         <script type="text/javascript" src="js/lib.js"></script>
         <script type="text/javascript" src="js/app.js"></script>
         <script type="text/javascript" src="js/autocomplete.js"></script>
